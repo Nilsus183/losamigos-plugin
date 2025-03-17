649ece92a08311e64849a022f37779712db0977c
Nils <155683736+Nilsus183@users.noreply.github.com>
2025-03-17 19:21:15 +0100

    Add files via upload

--- losamigos-plugin.php
+++ losamigos-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: QR Codes & Medals
- * Description: Fügt die Custom Post Types "QR Codes" und "Medaillen" hinzu, inklusive ACF-Felder und REST-API-Schnittstellen.
+ * Plugin Name: Artists & Medals
+ * Description: Fügt die Custom Post Types "Künstler" und "Medaillen" hinzu, inklusive ACF-Felder und REST-API-Schnittstellen.
  * Version: 1.0
  * Author: Nils
  * License: GPL2
@@ -12,13 +12,13 @@ if (!defined('ABSPATH')) {
 }
 
 // Custom Post Types registrieren
-function create_qr_code_post_type() {
-    register_post_type('qr_codes', array(
+function create_custom_post_type() {
+    register_post_type('artists', array(
         'labels' => array(
-            'name' => __('QR Codes'),
-            'singular_name' => __('QR Code'),
-            'add_new_item' => __('Neuen QR Code erstellen'),
-            'edit_item' => __('QR Code bearbeiten')
+            'name' => __('Künstler'),
+            'singular_name' => __('Künstler'),
+            'add_new_item' => __('Neuen Künstler hinzufügen'),
+            'edit_item' => __('Künstler bearbeiten')
         ),
         'public' => true,
         'show_in_rest' => true,
@@ -38,34 +38,28 @@ function create_qr_code_post_type() {
         'menu_icon' => 'dashicons-awards',
     ));
 }
-add_action('init', 'create_qr_code_post_type');
+add_action('init', 'create_custom_post_type');
 
 // ACF-Felder registrieren
 function register_acf_fields() {
     if (function_exists('acf_add_local_field_group')) {
         acf_add_local_field_group(array(
-            'key' => 'group_qr_code_info',
-            'title' => 'QR Code Informationen',
+            'key' => 'group_artist_info',
+            'title' => 'Künstler Informationen',
             'fields' => array(
                 array(
                     'key' => 'field_code',
                     'name' => 'code',
-                    'label' => 'Eindeutiger Code',
+                    'label' => 'Eindeutiger Künstler-Code',
                     'type' => 'text',
                     'required' => 1,
-                ),
-                array(
-                    'key' => 'field_artist',
-                    'name' => 'artist',
-                    'label' => 'Künstler',
-                    'type' => 'text',
                 )
             ),
             'location' => array(
                 array(array(
                     'param' => 'post_type',
                     'operator' => '==',
-                    'value' => 'qr_codes',
+                    'value' => 'artists',
                 )),
             ),
         ));
@@ -115,7 +109,7 @@ function register_acf_fields() {
 add_action('acf/init', 'register_acf_fields');
 
 // REST-API Erweiterungen
-add_filter('rest_qr_codes_query', function($args, $request) {
+add_filter('rest_artists_query', function($args, $request) {
     $code = $request->get_param('meta_value');
     if ($code) {
         $args['meta_query'] = [[
@@ -128,15 +122,53 @@ add_filter('rest_qr_codes_query', function($args, $request) {
 }, 10, 2);
 
 // REST-API Route für QR-Code Count
-function get_total_qr_codes_count() {
-    $count_posts = wp_count_posts('qr_codes');
+function get_total_artists_count() {
+    $count_posts = wp_count_posts('artists');
     return isset($count_posts->publish) ? $count_posts->publish : 0;
 }
 
+function get_scanned_artists($request) {
+    $codes = $request->get_param('codes');
+    if(!$codes) return array();
+
+    $args = array(
+        'post_type' => 'artists',
+        'meta_query' => array(
+            array(
+                'key' => 'code',
+                'value' => $codes,
+                'compare' => 'IN'
+            )
+        )
+    );
+
+    $query = new WP_Query($args);
+    $artists = array();
+
+    if($query->have_posts()) {
+        while($query->have_posts()) {
+            $query->the_post();
+            $artists[] = array(
+                'title' => get_the_title(),
+                'content' => wp_strip_all_tags(apply_filters('the_content', get_the_content())),
+                'code' => get_post_meta(get_the_ID(), 'code', true)
+            );
+        }
+    }
+    
+    wp_reset_postdata();
+    return $artists;
+}
+
 add_action('rest_api_init', function() {
-    register_rest_route('wp/v2', '/qr-codes-count', array(
+    register_rest_route('wp/v2', '/artists-count', array(
         'methods'  => 'GET',
-        'callback' => 'get_total_qr_codes_count'
+        'callback' => 'get_total_artists_count'
+    ));
+
+    register_rest_route('wp/v2', '/scanned-artists', array(
+        'methods'  => 'GET',
+        'callback' => 'get_scanned_artists',
     ));
 });
 
